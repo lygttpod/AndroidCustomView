6cb4418d4819c84d36d1c32736d7e2e84a5340b1
Allen <lygttpod@163.com>
2021-01-28 14:09:15 +0800

    完善demo

--- app/src/main/java/com/allen/androidcustomview/activity/ClearScreenActivity.kt
+++ app/src/main/java/com/allen/androidcustomview/activity/ClearScreenActivity.kt
@@ -2,7 +2,10 @@ package com.allen.androidcustomview.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.allen.androidcustomview.R
+import com.allen.androidcustomview.widget.ClearScreenMode
+import com.allen.androidcustomview.widget.ClearScreenView
 import kotlinx.android.synthetic.main.activity_clear_screen.*
 
 /**
@@ -13,11 +16,25 @@ import kotlinx.android.synthetic.main.activity_clear_screen.*
  *      desc    :
  * </pre>
  */
-class ClearScreenActivity : AppCompatActivity() {
+class ClearScreenActivity : AppCompatActivity(), ClearScreenView.OnClearScreenListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clear_screen)
+
         clear_screen_container.addClearView(iv_clear_content)
+
+        clear_screen_container.setOnClearScreenListener(this)
+
+        btn_quick_clear.setOnClickListener { clear_screen_container.clearScreenMode = ClearScreenMode.QUICK_SCROLL }
+        btn_slow_clear.setOnClickListener { clear_screen_container.clearScreenMode = ClearScreenMode.SLOW_SCROLL }
+    }
+
+    override fun onCleared() {
+        Toast.makeText(this, "清屏了", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestored() {
+        Toast.makeText(this, "恢复了", Toast.LENGTH_SHORT).show()
     }
 }
